e34050e408063f24b99d84145fd34a8a983d16a6
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2020-03-07 20:27:24 -0600

    REMOVING UNNECESSARY FILES

--- public/javascripts/gallery.js
+++
@@ -1,4 +0,0 @@
-baguetteBox.run('.jv-gallery');
-baguetteBox.run('.tp-gallery');
-baguetteBox.run('.mus-gallery');
-baguetteBox.run('.tr-gallery');
--- public/javascripts/programas.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-//   $('body').scrollspy({target: "#sideMenu", offset: 50});  
-// });
-// $(window).on('scroll', function(event) {
-//   var scrollValue = $(window).scrollTop();
-//   if (scrollValue == settings.scrollTopPx || scrollValue > 70) {
-//        $('.navbar').addClass('fixed-top');
-//   } 
-// }); 
